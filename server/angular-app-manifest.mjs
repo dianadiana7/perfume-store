b321cd0d1f725900764dda29b4787ef2a2e632d4
Diana <diana2962002@gmail.com>
2025-07-02 19:23:57 +0300

    Deploy properly to gh-pages

---
+++ server/angular-app-manifest.mjs
@@ -0,0 +1,507 @@
+
+export default {
+  bootstrap: () => import('./main.server.mjs').then(m => m.default),
+  inlineCriticalCss: true,
+  baseHref: '/',
+  locale: undefined,
+  routes: [
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-TBR3OSIS.js"
+    ],
+    "route": "/"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-OZ66WUGS.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/fragrances"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/3"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/4"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/5"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/6"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/7"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/8"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/9"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/10"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/51"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/52"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/53"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/54"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/55"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/56"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/57"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/58"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/59"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/60"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/61"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/62"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/17"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/18"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/19"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/20"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/21"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/12"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/13"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/14"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/11"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/24"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/25"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/26"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/27"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/28"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/29"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/32"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/34"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/35"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/36"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/37"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/38"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/41"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/42"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/43"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/44"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/45"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/46"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/47"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/48"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/49"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/50"
+  },
+  {
+    "renderMode": 0,
+    "preload": [
+      "chunk-55XRYKKR.js",
+      "chunk-ZVL34QGZ.js"
+    ],
+    "route": "/product/*"
+  },
+  {
+    "renderMode": 2,
+    "preload": [
+      "chunk-BXHZYVQB.js"
+    ],
+    "route": "/cart"
+  }
+],
+  entryPointToBrowserMapping: undefined,
+  assets: {
+    'index.csr.html': {size: 700, hash: 'eaeafad63a657151924ab24fac1e7ce7d73d72a52292b5d4993275fe3d5c32db', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1213, hash: '98f129dcc6f77526cb112ceabf8a493234aade91e838e5f55eff80e1bccdb525', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 8618, hash: '189d4a44f1d18fda7098916f612014e184b608ae09b9d7a7be17e7bfac50e209', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'product/5/index.html': {size: 8463, hash: '155e9846674dc9577ebbf22bc3fc90624c0350c2fa762e217f55439c05e2a6c9', text: () => import('./assets-chunks/product_5_index_html.mjs').then(m => m.default)},
+    'fragrances/index.html': {size: 25993, hash: '0d24aefd0bb607c870308706f486efe0ef334bfe266854c90afdd199645c620a', text: () => import('./assets-chunks/fragrances_index_html.mjs').then(m => m.default)},
+    'product/3/index.html': {size: 8419, hash: 'e35961ce6292f2824d78b7d76cdffd7b0949ff54262764abbcbbcf9892e28a99', text: () => import('./assets-chunks/product_3_index_html.mjs').then(m => m.default)},
+    'product/7/index.html': {size: 8435, hash: 'f62e7f8d29839ce425baf510376157f541b1e61a13c1115a625663177fbcada8', text: () => import('./assets-chunks/product_7_index_html.mjs').then(m => m.default)},
+    'product/6/index.html': {size: 8450, hash: '92db40e604a4e7b8d8e5371240c32ac5481007188dd817cbf9de86cf8845ac80', text: () => import('./assets-chunks/product_6_index_html.mjs').then(m => m.default)},
+    'product/4/index.html': {size: 8436, hash: '754e5d236f4a0609876302d216d645820386f9b1fb7023dd2fc1643a2fc0a894', text: () => import('./assets-chunks/product_4_index_html.mjs').then(m => m.default)},
+    'product/9/index.html': {size: 8439, hash: '2966799ccd17027587fcdd6955a54fbd107d5fcee0ce901fef78f1b37afe1a26', text: () => import('./assets-chunks/product_9_index_html.mjs').then(m => m.default)},
+    'product/10/index.html': {size: 8516, hash: '82aa955f239f1837c1779e84654255a89a5c219e539f2d96257ee5201ca67084', text: () => import('./assets-chunks/product_10_index_html.mjs').then(m => m.default)},
+    'product/8/index.html': {size: 8439, hash: '6a2360d4daac9543dff4388b8a7fafda44ede434b91c21226ccfd6ccee95300d', text: () => import('./assets-chunks/product_8_index_html.mjs').then(m => m.default)},
+    'product/52/index.html': {size: 8329, hash: 'edb6b20d3d85c10a2aff33091772d5520f039f36e500036904ae762955b515a0', text: () => import('./assets-chunks/product_52_index_html.mjs').then(m => m.default)},
+    'product/53/index.html': {size: 8385, hash: '93c1ee1cfcf977ba08271f593b85f4338e327f1dc71ebbaaf72a5d68ebfdd7b9', text: () => import('./assets-chunks/product_53_index_html.mjs').then(m => m.default)},
+    'product/54/index.html': {size: 8378, hash: '3e13ca6a630e6d0da12165447b63ba679835a36ff696a3b05f3f41511a59b720', text: () => import('./assets-chunks/product_54_index_html.mjs').then(m => m.default)},
+    'product/51/index.html': {size: 8400, hash: '556e3055faa6da473880ce42ebda4469d9d552629cff11e748977f54a011bbe7', text: () => import('./assets-chunks/product_51_index_html.mjs').then(m => m.default)},
+    'product/55/index.html': {size: 8393, hash: '4e955d1fc72fb6036cd98e684b9b0b2a5334110b333cb831fbbaebd18e17f454', text: () => import('./assets-chunks/product_55_index_html.mjs').then(m => m.default)},
+    'product/56/index.html': {size: 8371, hash: '6bae71a2519a91700fa99051096bf634f194ea590dff99f556a8e71fc7d21029', text: () => import('./assets-chunks/product_56_index_html.mjs').then(m => m.default)},
+    'product/59/index.html': {size: 8382, hash: '9d7af2c89c825d489109ad7d929f7ac49f0a34347df8b4eb2f2bc0e93e2b65bc', text: () => import('./assets-chunks/product_59_index_html.mjs').then(m => m.default)},
+    'product/57/index.html': {size: 8360, hash: '310ef1e8c68f974de969c9989a0eeb3d9bceaaafedf1850c7d1893dd131fb87e', text: () => import('./assets-chunks/product_57_index_html.mjs').then(m => m.default)},
+    'product/58/index.html': {size: 8484, hash: '49434b6566ddd0304d075ebe1275b01462e6e0ccdf9ed954c77679354b488125', text: () => import('./assets-chunks/product_58_index_html.mjs').then(m => m.default)},
+    'product/62/index.html': {size: 8281, hash: '4c1f0a7a2cc70012d2be08d309b75336d771baee34ded54d2b76af76db494863', text: () => import('./assets-chunks/product_62_index_html.mjs').then(m => m.default)},
+    'product/60/index.html': {size: 8371, hash: '4f7af342462e03fd85cae5749ee3dd0c491dcd68563ecd8f6c85711fa806fd24', text: () => import('./assets-chunks/product_60_index_html.mjs').then(m => m.default)},
+    'product/61/index.html': {size: 8317, hash: '6c7e78723bd0ae2c9c9d04a37957e44c4628769e450c869304cb87683eb8c169', text: () => import('./assets-chunks/product_61_index_html.mjs').then(m => m.default)},
+    'product/17/index.html': {size: 8437, hash: '1f0a3a06126e3f414dfab35ab7c7cbe2dfc1a726825adea9cfa40b22a76a4c8a', text: () => import('./assets-chunks/product_17_index_html.mjs').then(m => m.default)},
+    'product/19/index.html': {size: 8600, hash: 'bfb2019e25c3b74e6f765d8ff7b75fc0572e601c2ed1c07a2e121eae82c5c585', text: () => import('./assets-chunks/product_19_index_html.mjs').then(m => m.default)},
+    'product/20/index.html': {size: 8483, hash: 'ad73c52eb901eb6a117dd0770738cec3e0e645f21bbefc45ae7628ef65382f4a', text: () => import('./assets-chunks/product_20_index_html.mjs').then(m => m.default)},
+    'product/21/index.html': {size: 8424, hash: '429e39ac158d82080acd30c96286dec16fec4fa791c03a2d7dc2e7b232c367e3', text: () => import('./assets-chunks/product_21_index_html.mjs').then(m => m.default)},
+    'product/18/index.html': {size: 8424, hash: 'a7ebe86763411caab0f77cdc18b5ca5ecbf1576e4b92d0e03a3dcb1268544a25', text: () => import('./assets-chunks/product_18_index_html.mjs').then(m => m.default)},
+    'product/12/index.html': {size: 8337, hash: 'd677100291601dd4d30fd02ba9255fa57a455c08f61b8ef11b2a518a22224837', text: () => import('./assets-chunks/product_12_index_html.mjs').then(m => m.default)},
+    'product/11/index.html': {size: 8450, hash: 'e1b1cf0b6609169f8e7dc15115934cb42ea5d19f468f9f65bf1b8367ee533440', text: () => import('./assets-chunks/product_11_index_html.mjs').then(m => m.default)},
+    'product/14/index.html': {size: 8345, hash: '8b2e3719dacd30006a36effe3465b6ff3149a796bfd2bb66221cbfe9e59ef395', text: () => import('./assets-chunks/product_14_index_html.mjs').then(m => m.default)},
+    'product/13/index.html': {size: 8340, hash: '50bb6ca50facac6d6e7b8266c9593c4b78b23bfbd9f8d323ded3d606dd53835a', text: () => import('./assets-chunks/product_13_index_html.mjs').then(m => m.default)},
+    'product/25/index.html': {size: 8387, hash: '99d73fe96405b0f3a2ea15197296ab1535aeafc3d373807d8d1bcfda7ef2f1f9', text: () => import('./assets-chunks/product_25_index_html.mjs').then(m => m.default)},
+    'product/26/index.html': {size: 8396, hash: '550ad312beaa0fe14c0c72c5823d07bc07dea61c4e3173b283f34bda99ef52f3', text: () => import('./assets-chunks/product_26_index_html.mjs').then(m => m.default)},
+    'product/24/index.html': {size: 8384, hash: '60ee9f73a13800c10897062cf99b820c7bcbf9fa1c57df74116a2a04a75c18db', text: () => import('./assets-chunks/product_24_index_html.mjs').then(m => m.default)},
+    'product/27/index.html': {size: 8392, hash: 'ca6f67a35f3fb0852a1233df458a7399eb2eb4f0f76eb91cda0b81ccef857509', text: () => import('./assets-chunks/product_27_index_html.mjs').then(m => m.default)},
+    'product/32/index.html': {size: 8339, hash: '12ee95141fab311322e894386fa599fca6cc5a44cd90b4f8824928b11b5e92c2', text: () => import('./assets-chunks/product_32_index_html.mjs').then(m => m.default)},
+    'product/29/index.html': {size: 8293, hash: '8734cf7fbcf34074e46a22ddc4f58007b09fd8fcd1e3fd2503398fc6373b6b9c', text: () => import('./assets-chunks/product_29_index_html.mjs').then(m => m.default)},
+    'product/34/index.html': {size: 8410, hash: '19f20391d90621635ba1da544d3b784b1dab6fb554e431725731e5014f8d0cfa', text: () => import('./assets-chunks/product_34_index_html.mjs').then(m => m.default)},
+    'product/28/index.html': {size: 8286, hash: '64adf6a9c4e46729144b654804b331981d3daff2424e6444ebdc5275bce2027f', text: () => import('./assets-chunks/product_28_index_html.mjs').then(m => m.default)},
+    'product/37/index.html': {size: 8397, hash: '365d2535b1563504c9b145e94d4efee9d480e825db29818646474663cb973247', text: () => import('./assets-chunks/product_37_index_html.mjs').then(m => m.default)},
+    'product/35/index.html': {size: 8311, hash: '0132a5e97c98d0d51c578819c158905bd44604ad5a9f718207b8f18004fc7a68', text: () => import('./assets-chunks/product_35_index_html.mjs').then(m => m.default)},
+    'product/38/index.html': {size: 8381, hash: '203b0242a5666d87d69d2c10a028acc2e54b7294bfd152b7622734a9dba45cd2', text: () => import('./assets-chunks/product_38_index_html.mjs').then(m => m.default)},
+    'product/36/index.html': {size: 8369, hash: '3ac25b718f0d935ec362df613d863fa8fad17dbfc573d1560eed3c8d35559a37', text: () => import('./assets-chunks/product_36_index_html.mjs').then(m => m.default)},
+    'product/42/index.html': {size: 8308, hash: '999ee901f271ae4a4560b426139e411312d80031c805365ba7bd3038413656f5', text: () => import('./assets-chunks/product_42_index_html.mjs').then(m => m.default)},
+    'product/41/index.html': {size: 8307, hash: '7d9161ebf45a246a4df07ead443781a3c571c793402d01d323cd970351c56759', text: () => import('./assets-chunks/product_41_index_html.mjs').then(m => m.default)},
+    'product/43/index.html': {size: 8311, hash: '6eb103aa8ebf54487fb975f431146f2a3699fb782f9fd57e65bc5268be34d876', text: () => import('./assets-chunks/product_43_index_html.mjs').then(m => m.default)},
+    'product/44/index.html': {size: 8354, hash: '36b9181732b32dab9595bb3b059b66609c28bc6ef096a66561ba218ed04ddca3', text: () => import('./assets-chunks/product_44_index_html.mjs').then(m => m.default)},
+    'product/45/index.html': {size: 8335, hash: '90bf876b38b58ed35a886c8e062b1a9c0cda99e6686666c4225900c6164dc553', text: () => import('./assets-chunks/product_45_index_html.mjs').then(m => m.default)},
+    'product/46/index.html': {size: 8448, hash: '7705626403e6dc3ac5437487c4e09ad25886b44658a2aed284b14ed527d11f6d', text: () => import('./assets-chunks/product_46_index_html.mjs').then(m => m.default)},
+    'product/50/index.html': {size: 8397, hash: '3a576091ebf1fbef4e4616ff501bb535dd100d005887331b323e2db3bce8614e', text: () => import('./assets-chunks/product_50_index_html.mjs').then(m => m.default)},
+    'product/49/index.html': {size: 8361, hash: '70d1a7c164d6e0f60ccef0da2c249f2b0a5ac474df086b700411fcba6d7ce9e2', text: () => import('./assets-chunks/product_49_index_html.mjs').then(m => m.default)},
+    'product/47/index.html': {size: 8368, hash: '9d4975d7233f285b173d38a0c3d28e0f0fea867937a721981aa0fb5bb6a6fa81', text: () => import('./assets-chunks/product_47_index_html.mjs').then(m => m.default)},
+    'product/48/index.html': {size: 8350, hash: '81ae64d71ba6da3f6e0c7d8c7773709825c66c093aff70ed3d12b226c1e37333', text: () => import('./assets-chunks/product_48_index_html.mjs').then(m => m.default)},
+    'cart/index.html': {size: 6687, hash: '111c08abbaccaa71c202dbd9525c5e9bf0fcd42030662e35ef39ac595aaf840e', text: () => import('./assets-chunks/cart_index_html.mjs').then(m => m.default)},
+    'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
+  },
+};
---
+++ server/assets-chunks/index_csr_html.mjs
@@ -0,0 +1,14 @@
+export default `<!doctype html>
+<html lang="en" data-beasties-container="">
+<head>
+  <meta charset="utf-8">
+  <title>PerfumeStore</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="styles-5INURTSO.css"></head>
+<body ngcm="">
+  <app-root></app-root>
+<link rel="modulepreload" href="chunk-FNZY4PHM.js"><link rel="modulepreload" href="chunk-IUTZEPZ4.js"><link rel="modulepreload" href="chunk-4BEZIMBZ.js"><link rel="modulepreload" href="chunk-OCS2N2NT.js"><script src="polyfills-B6TNHZQ6.js" type="module"></script><script src="main-OTEFXFGY.js" type="module"></script></body>
+</html>
+`;
